dbd699ce7264b0f650b9dec5af325db212f0b740
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-09-22 17:21:27 -0700

    Rewrite listener logic

--- script.js
+++ script.js
@@ -1,81 +1,112 @@
 const gameBoard = (() => {
-    
-    const boardArray = [];
+
+    const boardRows = [];
     const dimension = 3;
+
     for (let i = 0; i < dimension; i++) {
         const row = [];
-        for (let j = 0; j < dimension; j++) {
-            row.push(" ");
-        }
-        boardArray.push(row);
-    }
-    return boardArray;
-})();
 
+        for (let j = 0; j < dimension; j++) {
+            row.push("");
+        }
+
+        boardRows.push(row);
+    }
+
+    return boardRows;
+})();
 
 const displayController = (() => {
 
+    const clickListener = (cell, i, j) => {
+
+        cell.addEventListener('click', (e) => {
+                
+            e.preventDefault();
+
+            e.target.textContent = "o";
+            console.log(e.target);
+            e.target.setAttribute('disabled', '');
+            if (!gameBoard[i][j]) {
+                gameBoard[i][j] = "o";
+            }
+        });
+    };
+
     const gameboardContainer = document.getElementsByClassName("gameboardContainer")[0];
 
-    for (let i = 0; i < gameBoard.length; i++) {
+    for (let i = 0, len = gameBoard.length; i < len; i++) {
 
-        for (let j = 0; j < gameBoard[i].length; j++){
+        for (let j = 0, jlen = gameBoard[i].length; j < jlen; j++) {
 
-            const cell = document.createElement("div");
+            const cell = document.createElement('button');
             cell.textContent = gameBoard[i][j];
             cell.classList.add("cell");
             gameboardContainer.appendChild(cell);
-        }     
+            
+            clickListener(cell, i, j);
+        }
     }
+
 })();
 
 
-function Player(input) {
+// function Player(input) {
 
-    const move = () => input;
-    const playerMove = () => {
+//     const move = () => input;
+//     const playerMove = () => {
 
-        const cells = Array.from(document.getElementsByClassName("cell"));
-        cells.forEach(cell => {
+//         const cells = Array.from(document.getElementsByClassName("cell"));
+//         cells.forEach(cell => {
 
-            cell.addEventListener("click", () => {
+//             cell.addEventListener("click", () => {
 
-                if (cell.textContent != "") {
-                    return;
-                }
+//                 if (cell.textContent != "") {
+//                     return;
+//                 }
 
-                cell.textContent = move();
-                togglePlayers();
-            });
-        });
-    }
-    return {playerMove};
-}
+//                 console.log("clicked!")
 
-const preparePlayers = (() => {
+//                 cell.textContent = move();
+//                 // togglePlayers();
+//             });
+//         });
+//     }
+//     return {playerMove};
+// }
 
-    const player1 = Player("o");
-    const player2 = Player("x");
-    const state = {
-        player1Playing: true,
-        player1,
-        player2,
-    }
+// const preparePlayers = (() => {
 
-    return state;
-})();
+//     const player1 = Player("o");
+//     // const player2 = Player("x");
+//     player1.playerMove();
+//     // player2.playerMove();
 
-function togglePlayers () {
+//     const state = {
 
-    if(preparePlayers.state.player1Playing) {
+//         player1Playing: true,
+//         player1,
+//         // player2,
+//     }
 
-        preparePlayers.state.player1Playing = false;
+//     //test
+//     console.log("Pass");
 
-    } else {
+//     return state;
+// })();
 
-        preparePlayers.state.player1Playing = true;
+// function togglePlayers () {
 
-    }
-}
+//     if(preparePlayers.state.player1Playing) {
+
+//         preparePlayers.state.player1Playing = false;
+
+//     } else {
+
+//         preparePlayers.state.player1Playing = true;
+
+//     }
+// }
 
 // const gameState = preparePlayers();
+// Player.playerMove();
