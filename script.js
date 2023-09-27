940d61eb881556123a7ba2219ccd70fc897a1521
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-09-27 10:53:29 -0700

    Add simple game-end logic

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const createPlayer = (move) => {
+
+    const isPlaying = false;
+    return { move, isPlaying };
+}
+
 const gameBoard = (() => {
 
     const boardRows = [];
@@ -18,22 +24,90 @@ const gameBoard = (() => {
 
 const displayController = (() => {
 
+    const gameboardContainer = document.getElementsByClassName("gameboardContainer")[0];
+
+    const player1 = createPlayer('o');
+    player1.isPlaying = true;
+    const player2 = createPlayer('x');
+
+    const hasGameEnded = (i, j) => {
+
+        let verticalCheck = true, horizontalCheck = true;
+        
+        for (let x = 0, dimension = gameBoard.length; x < (dimension - 1) && (horizontalCheck == true || verticalCheck == true); x++) {
+            
+            if (gameBoard[i][x] == '') {
+                verticalCheck = false;
+            }
+            if (gameBoard[i][x] != gameBoard[i][x + 1]) {
+                verticalCheck = false;
+            }
+
+            if (gameBoard[x][j] == '') {
+                horizontalCheck = false;
+            }
+            if (gameBoard[x][j] != gameBoard[x + 1][j]) {
+                horizontalCheck = false;
+            }
+
+            if (verticalCheck == false && horizontalCheck == false) {
+                return false;
+            }
+            ////////////////////// still need to check across
+        }
+        console.log('winner!');
+        return true;
+    }
+
+    const togglePlayers = () => {
+
+        player1.isPlaying = !player1.isPlaying;
+        player2.isPlaying = !player2.isPlaying;
+
+    }
+
+    const moveToUse = () => {
+ 
+        if (player1.isPlaying) {
+            return player1.move;
+        } else {
+            return player2.move;
+        }
+    }
+
+    const disableBoard = () => {
+
+        const cells = gameboardContainer.childNodes;
+        console.log(cells);
+        cells.forEach(cell => {
+            cell.setAttribute('disabled', '');
+        });
+    }
+
     const clickListener = (cell, i, j) => {
 
         cell.addEventListener('click', (e) => {
                 
             e.preventDefault();
 
-            e.target.textContent = "o";
-            console.log(e.target);
+            const move = moveToUse();
+
+            e.target.textContent = move;
             e.target.setAttribute('disabled', '');
             if (!gameBoard[i][j]) {
-                gameBoard[i][j] = "o";
+
+                gameBoard[i][j] = move;
             }
+
+            if (hasGameEnded(i, j)) {
+
+                disableBoard();
+                ////////////////////////// need to announce winner
+            }
+
+            togglePlayers();
         });
     };
-
-    const gameboardContainer = document.getElementsByClassName("gameboardContainer")[0];
 
     for (let i = 0, len = gameBoard.length; i < len; i++) {
 
@@ -51,7 +125,9 @@ const displayController = (() => {
 })();
 
 
-// function Player(input) {
+
+
+// function createPlayer(input) {
 
 //     const move = () => input;
 //     const playerMove = () => {
@@ -77,8 +153,8 @@ const displayController = (() => {
 
 // const preparePlayers = (() => {
 
-//     const player1 = Player("o");
-//     // const player2 = Player("x");
+//     const player1 = createPlayer("o");
+//     // const player2 = createPlayer("x");
 //     player1.playerMove();
 //     // player2.playerMove();
 
@@ -109,4 +185,4 @@ const displayController = (() => {
 // }
 
 // const gameState = preparePlayers();
-// Player.playerMove();
+// createPlayer.playerMove();
