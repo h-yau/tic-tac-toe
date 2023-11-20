166ea50aa8f9426a8a827c2c4402cae06372d732
nzxzn <99358826+nzxzn@users.noreply.github.com>
2023-11-20 13:08:37 -0800

    Rewrite winning conditions

--- script.js
+++ script.js
@@ -71,47 +71,12 @@ const gameBoard = (() => {
         let moveToCheck = activePlayer.playMove();
 
         const winningConditions = [
-            [
-                [moveToCheck, moveToCheck, moveToCheck],
-                ["", "", ""],
-                ["", "", ""]
-            ],
-            [
-                ["", "", ""],
-                [moveToCheck, moveToCheck, moveToCheck],
-                ["", "", ""]
-            ],
-            [
-                ["", "", ""],
-                ["", "", ""],
-                [moveToCheck, moveToCheck, moveToCheck],
-            ],
-            [
-                [moveToCheck, "", ""],
-                [moveToCheck, "", ""],
-                [moveToCheck, "", ""]
-            ],
-            [
-                ["", moveToCheck, ""],
-                ["", moveToCheck, ""],
-                ["", moveToCheck, ""]
-            ],
-            [
-                ["", "", moveToCheck],
-                ["", "", moveToCheck],
-                ["", "", moveToCheck]
-            ],
-            [
-                ["", "", moveToCheck],
-                ["", moveToCheck, ""],
-                [moveToCheck, "", ""]
-            ],
-            [
-                [moveToCheck, "", ""],
-                ["", moveToCheck, ""],
-                ["", "", moveToCheck]
-            ]
+            [board[0][0], board[0][1], board[0][2]],
+            [board[1][0], board[1][1], board[1][2]],
+            [board[2][0], board[2][1], board[2][2]],
+            
         ]
+        ;
     }
 
     const isTied = () => {
