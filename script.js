cc2a26c932e995c16ade524c582556c481da79b3
nzxzn <99358826+nzxzn@users.noreply.github.com>
2023-11-30 15:22:42 -0800

    Rewrite logic

--- script.js
+++ script.js
@@ -1,188 +1,49 @@
-const player = (isItPlayer1) => {
-    let mark = "x";
-    let name = "Player 2";
-    if(isItPlayer1 == true) {
-        mark = "o";
-        name = "Player 1";
-    }
+const player = (sign) => {
+
+
     const playMove = () => {
-        // e.target.classlist.add(mark);
-        return mark;
-    }
-    return {playMove, name};
+        return sign;
+    };
+    
+    return {playMove};
 };
 
-const gameBoard = (() => {
+const gameController = (() => {
+    const player1 = player("o");
+    const player2 = player("x");
+    let isPlayer1sTurn = true;
+
+    const gameArray = [];
     const DIMENSION = 3;
-    const board = [];
 
-    // to create empty 2D array
     for (let i = 0; i < DIMENSION; i++) {
-        board[i] = ["", "" , ""];
-    };
-
-    const getBoard = () => board;
-    const updateBoard = (coordinates, move) => {
-        const [x, y] = coordinates;
-        if (board[x][y] == false || board[x][y] == null || board[x][y] == undefined) {
-            board[x][y] = move;
-            return true;
-        }
-        return false;
-    } 
-
-    // to set up players
-    const player1 = player(true);
-    const player2 = player(false);
-    let hasGameEnded = false;  
-
-    let activePlayer = player2;
-
-    const toggleActivePlayer = () => {
-        return activePlayer == player1 ? player2 : player1;
+        gameArray.push(["", "", ""]); 
     }
 
-    let isFirstRound = true;
-
-    const playRound = (coordinates, currentMove) => {
-        // let coordinates;
-        // if (activePlayer == player1) {
-        //     coordinates = prompt("Player 1's turn: ");
-        // } else {
-        //     coordinates = prompt("Player 2's turn: ");
-        // }
-
-        let isUpdateSuccessful = updateBoard(coordinates, activePlayer.playMove());
-
-        // to check repeated move on a single cell
-        if (!isUpdateSuccessful) {
-            console.log("Update not successful!");
-            return;
-        }
-
-        // to console the table after every move, legal or not
-        console.table(getBoard());
-        if (isThereAWinner()) {
-            console.log(`${activePlayer.name} wins!`);
-            hasGameEnded = true;
-        }
-
-        // after checking winner, check if it's tied
-        else if (isTied()) {
-            console.log("It's a tie!");
-            hasGameEnded = true;
-        }
-
-        // prompt to restart
-        if (hasGameEnded) {
-            let isToRestart = prompt("Do you want to restart game? Answer yes or no.");
-            if (isToRestart == "yes") {
-                restartGame();
+    const clearGameArray = () => {
+        for (let i = 0; i < DIMENSION; i++) {
+            for (let j = 0; j < DIMENSION; j++) {
+                gameArray[i][j] = "";
             }
         }
-
-        if (isFirstRound) {
-            isFirstRound = false;            
-        }
-        activePlayer = toggleActivePlayer();
-        console.log(activePlayer);
-    };
-
-    const isThereAWinner = () => {
-        let moveToCheck = activePlayer.playMove();
-
-        const winningConditions = [
-            [board[0][0], board[0][1], board[0][2]],
-            [board[1][0], board[1][1], board[1][2]],
-            [board[2][0], board[2][1], board[2][2]],
-            [board[0][0], board[1][0], board[2][0]],
-            [board[1][0], board[1][1], board[1][2]],
-            [board[2][0], board[2][1], board[2][2]],
-            [board[0][0], board[1][1], board[2][2]],
-            [board[0][2], board[1][1], board[2][0]]
-        ];
-
-        return winningConditions.some(condition => {
-            return condition.every(cell => cell == moveToCheck);
-        });
     }
 
-    const isTied = () => {
-        return board.every(row => {
-            return row.every(cell => cell == 'o' || cell == "x");
-        });
-    }
-
-    const restartGame = () => {
+    const addMove = (coordinates, currentPlayer) => {
+        const [row, col] = coordinates;
         
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                board[i][j] = "";
-            }
+        // if cell is already occupied 
+        if (gameArray[row][col] !== "") {
+            return false;
         }
-
-
-        activePlayer = player1;
-        isFirstRound = true;
-        console.table(gameBoard.getBoard());
-        startGame();
-    }
-
-
-    const startGame = () => {
-        while(!hasGameEnded) {
-            playRound();
-        }
-     };
-
-     // will turn on once it's the displaycontroller is completed
-    //  startGame();
-    return {getBoard, updateBoard, startGame, playRound, restartGame, activePlayer};
-})();
-
-const displayController = (() => {
-
-    let currentMove = gameBoard.activePlayer.playMove();
-
-    const gameboardContainer = document.getElementsByClassName("gameboardContainer")[0];
-
-    if (gameboardContainer != null || gameboardContainer != undefined) {
-        gameboardContainer.classList.add(currentMove);
-    }
-
-    const toggleDisplayGameBoard = () => {
-        if (gameboardContainer == null || gameboardContainer == undefined) {
-            return;
-        }
-        // somewhere around here? gameboardcontainer not changing
-        // somehow the two modules are not agreeing which is the activePlayer. DisplayController only prints out the default one (player1), while the one on top actually changes
-        gameboardContainer.classList.remove(currentMove);
-        console.log(currentMove);
-        currentMove = gameBoard.activePlayer.playMove();
-        console.log(currentMove);
-        gameboardContainer.classList.add(currentMove);
-
+        gameArray[row][col] = currentPlayer.playMove();
+        return true;
     };
 
-    const addMove = (e, index) => {
-  
-        e.preventDefault()
-        e.target.classList.add(currentMove);
-        console.log(Math.floor(index / 3), index % 3);
-        gameBoard.playRound([Math.floor(index / 3), index % 3], currentMove);
-    };
+    const togglePlayers = (isAddMoveSuccessful) {
+        if(isAddMoveSuccessful) {
+            isPlayer1sTurn = !isPlayer1sTurn;
+        }
+    }
 
-    // add listeners to the cells
-    const cells = document.querySelectorAll(".cell");
-    cells.forEach((cell, index) => {
-        cell.addEventListener('click', (e) => {
-            addMove(e, index);
-            console.log(gameBoard.activePlayer);
-            toggleDisplayGameBoard();
-
-        });
-    });
-
-
-
+    return {player1, player2, gameArray, addMove, clearGameArray};
 })();
